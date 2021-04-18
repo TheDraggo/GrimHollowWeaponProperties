7b1b0ed330412db9ca4633974155a56dac8c0bd2
TheDraggo <59175142+TheDraggo@users.noreply.github.com>
2021-04-18 23:50:00 +0200

    Add files via upload

---
+++ grimhollowwp.js
@@ -0,0 +1,10 @@
+Hooks.on('ready', () => {
+    CONFIG.DND5E.weaponProperties['blackpowder'] = 'Blackpowder';
+	CONFIG.DND5E.weaponProperties['brutal'] = 'Brutal';
+	CONFIG.DND5E.weaponProperties['cumbersome'] = 'Cumbersome';
+	CONFIG.DND5E.weaponProperties['guard'] = 'Guard';
+	CONFIG.DND5E.weaponProperties['momentum'] = 'Momentum';
+	CONFIG.DND5E.weaponProperties['repeater'] = 'Repeater';
+	CONFIG.DND5E.weaponProperties['swift'] = 'Swift';
+	CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+});
